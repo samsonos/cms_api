47f12169920abdd023ed9119a6e2b64c3e526a51
Kotenko <nick.w2r@gmail.com>
2015-05-19 16:11:59 +0300

    fix prepare creating user query

--- src/CMS.php
+++ src/CMS.php
@@ -352,7 +352,7 @@ class CMS extends CompressableService
 
         // Check if we did not already create user
         if (!sizeof(db()->fetch('SELECT * from user where email ="admin@admin.com"'))) {
-            db()->query("INSERT INTO `" . dbMySQLConnector::$prefix . "user` (`UserID`, `FName`, `SName`, `TName`, `email`, `md5_email`, `md5_password`, `created`, `modyfied`, `active`) VALUES
+            db()->query("INSERT INTO `" . dbMySQLConnector::$prefix . "user` (`user_id`, `f_name`, `s_name`, `t_name`, `email`, `md5_email`, `md5_password`, `created`, `modified`, `active`) VALUES
 	 (1, 'Виталий', 'Егоров', 'Игоревич', 'admin@admin.com', '64e1b8d34f425d19e1ee2ea7236d3028', '64e1b8d34f425d19e1ee2ea7236d3028', '2011-10-25 14:59:06', '2013-05-22 11:52:38',  1)
 			ON DUPLICATE KEY UPDATE active=1");
         }
